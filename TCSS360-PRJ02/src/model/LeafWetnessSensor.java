0ed52eee0478b37c1090144aed8697d54b5476b7
Albert Lin <abohowardlin@gmail.com>
2020-07-15 13:34:54 -0700

    added leaf wetness sensor and its test

---
+++ TCSS360-PRJ02/src/model/LeafWetnessSensor.java
@@ -0,0 +1,100 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Random;
+
+import application.Main;
+
+/**
+ * The Leaf Wetness sensor measures leaf wetness.
+ * The Leaf Wetness sensor runs on its own thread.
+ * 
+ * @author Albert Lin
+ * @version July 2020
+ *
+ */
+public class LeafWetnessSensor extends Thread implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/** minimum leaf wetness value in degrees */
+	public static final int MIN_LEAF_WETNESS = 0;
+	
+	/** maximum leaf wetness value in degrees */
+	public static final int MAX_LEAF_WETNESS = 15;
+	
+	/** the time interval between the data updates. */
+	public static final int INTERVAL = 54;
+	
+	/** an integer that represents the leaf wetness value the sensor generates. */
+	private int leafWetness; // updates 46 to 54 seconds
+	
+	/** A boolean to determine if the thread should sleep. */
+	private volatile boolean cancel;
+	
+	/** A Random object to generate data for the Rain Collector. */
+	private final Random rand;
+	
+	/**
+	 * constructor that initializes the variables. Initial leaf wetness is 0
+	 */
+	public LeafWetnessSensor() {
+		rand = new Random();
+		cancel = false;
+		leafWetness = 0;
+	}
+	
+	/**
+	 * Generates a random value for leaf wetness
+	 */
+	public void setLeafWetness() {
+		leafWetness = rand.nextInt(MAX_LEAF_WETNESS - MIN_LEAF_WETNESS + 1);
+	}
+	
+	/**
+	 * Returns the leaf wetness value
+	 * @return the leaf wetness value
+	 */
+	public int getLeafWetness() {
+		return leafWetness;
+	}
+	
+	/**
+	 * This is the run method that handles generates new data with the intervals.
+	 */
+	public void run() {
+	while (true) {
+		try {
+			if(!cancel) {
+				setLeafWetness();
+				Main.serialization("LeafWetness_S.txt", this);
+				
+				Thread.sleep(INTERVAL * 1000);
+			} else {
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+			System.out.println("Leaf Wetness Sensor stopped!");
+			} 
+		}
+	}
+	
+	/**
+	 * This is used to stop generating new data.
+	 */
+	public void cancel() {
+		this.cancel = true;
+	}
+	
+	/**
+	 * This is used to restart the data generation.
+	 */
+	public void restart() {
+		this.cancel = false;
+	}
+	
+	
+}
---
+++ TCSS360-PRJ02/src/tests/LeafWetnessSensorTest.java
@@ -0,0 +1,106 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import model.LeafWetnessSensor;
+
+/**
+ * A test class for the Leaf Wetness Sensor. The Leaf Wetness sensor measures leaf wetness 
+ * 
+ * @author Albert Lin
+ * @version July 2020
+ *
+ */
+class LeafWetnessSensorTest {
+	
+	/**
+	 * A Leaf Wetness Sensor object.
+	 */
+	private LeafWetnessSensor LWS;
+
+	/**
+	 * Initializes the Anemometer Sensor object and its thread
+	 * @throws Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		LWS = new LeafWetnessSensor();
+		LWS.start();
+	}
+	
+	/**
+	 * Tests if the anemometer sensor object is not null
+	 */
+	@Test
+	void testLeafWetnessSensor() {
+		assertNotNull(new LeafWetnessSensor());
+	}
+
+	/**
+	 * Tests if the thread is alive
+	 * @throws InterruptedException
+	 */
+	@Test
+	void testRun() throws InterruptedException {
+		assertTrue(LWS.isAlive());
+		
+		//uncomment when testing if data is updated when forced
+//		for (int i = 0; i < 10; i++) {
+//			LWS.setLeafWetness();
+//			System.out.println("Leaf Wetness: " + LWS.getLeafWetness());
+//			System.out.println();
+//			TimeUnit.SECONDS.sleep(3);
+//		}
+		
+		//uncomment when testing if data is automatically updated
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println("Leaf Wetness: " + LWS.getLeafWetness());
+//			System.out.println();
+//			TimeUnit.SECONDS.sleep(54);
+//		}
+	}
+	
+	/**
+	 * Tests if the leaf wetness measurements gotten are within the range of valid values. 
+	 * Implicitly tests the setter methods.
+	 */
+	@Test
+	void testGetLeafWetness() {
+		for (int i = 0; i < 1000; i++) {
+			LWS.setLeafWetness();
+			int leafWetness = LWS.getLeafWetness();		
+			assertTrue(leafWetness >= 0 && leafWetness <= 15, "leaf wetness must be within "
+					+ "0 and 15");
+		}	
+	}
+	
+	/**
+	 * This tests to see whether the cancel method keeps the thread alive.
+	 * @throws InterruptedException
+	 */
+	@Test
+	void testCancel() throws InterruptedException {
+		assertTrue(LWS.isAlive());
+		LWS.cancel();
+		TimeUnit.SECONDS.sleep(1);
+		assertTrue(LWS.isAlive());
+	}
+	
+	/**
+	 * This tests to see whether the restart method keeps the thread alive. 
+	 * @throws InterruptedException
+	 */
+	@Test
+	void testRestart() throws InterruptedException {
+		assertTrue(LWS.isAlive());
+		LWS.restart();
+		TimeUnit.SECONDS.sleep(1);
+		assertTrue(LWS.isAlive());
+	}
+
+}
